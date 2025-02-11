263d9451ecbc49c434646baaabcc9eb49bb6afc0
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-02-11 20:11:09 +0200

    add markup header

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,15 +1,27 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { images } from "./images.js";
+
+const images = [
+  {
+    preview: "path-to-preview-image1.jpg",
+    original: "path-to-original-image1.jpg",
+    description: "Image 1 description"
+  },
+  {
+    preview: "path-to-preview-image2.jpg",
+    original: "path-to-original-image2.jpg",
+    description: "Image 2 description"
+  }
+];
 
 const galleryContainer = document.querySelector(".gallery");
 
 const galleryMarkup = images
   .map(({ preview, original, description }) => `
     <li class="gallery-item">
-        <a class="gallery-link" href="${original}">
-            <img class="gallery-image" src="${preview}" alt="${description}" />
-        </a>
+      <a class="gallery-link" href="${original}">
+        <img class="gallery-image" src="${preview}" alt="${description}" />
+      </a>
     </li>
   `)
   .join("");
